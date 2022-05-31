0d5f62a204011dd8dc3bfdf44b07c7e26c333ae8
AlbertDzhamg <62603104+AlbertDzhamg@users.noreply.github.com>
2022-06-01 02:13:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Laba8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Company company = db.Companies.FirstOrDefault();
+                db.Entry(company).Collection(t => t.Users).Load();
+                Console.WriteLine($"Company: {company.Name}");
+                foreach (var p in company.Users)
+                    Console.WriteLine($"User: {p.Name}");
+            }
+
+        }
+    }
+    }
